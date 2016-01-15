4d0ec7051f455e0298c36df3c61147b583812877
mphipps <student@BLUC02MG32JFH51.local>
2016-01-15 10:11:02 -0600

    add names, delete rows

--- TableView/ViewController.swift
+++ TableView/ViewController.swift
@@ -21,6 +21,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         myTableView.dataSource = self
         myTableView.delegate = self
     }
+    
     //Required for tableview protocol - sends data to cell
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         //Creates the cell to be returned
@@ -30,9 +31,44 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         myCell.detailTextLabel!.text = realNames[indexPath.row]
         return myCell
     }
+    
     //Required for tableview protocol - sets num of rows in table
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return superheroes.count
+    }
+    
+    //Deletes cell
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath)
+    {
+        if editingStyle == .Delete
+        {
+            superheroes.removeAtIndex(indexPath.row)
+            realNames.removeAtIndex(indexPath.row)
+            myTableView.reloadData()
+        }
+    }
+    @IBAction func addButtonTapped(sender: UIBarButtonItem)
+    {
+        //Presents Alert view
+        let myAlert = UIAlertController(title: "Add Super Hero", message: nil, preferredStyle: .Alert)
+        myAlert.addTextFieldWithConfigurationHandler{(nameTextfield) -> Void in
+            nameTextfield.placeholder = "Add Super Hero Name"
+        }
+        myAlert.addTextFieldWithConfigurationHandler{(aliasTextfield) -> Void in
+            aliasTextfield.placeholder = "Add Super Hero Alias"
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
+        myAlert.addAction(cancelAction)
+        let addAction = UIAlertAction(title: "Add", style: .Default) { (addAction) -> Void in
+            let superHeroTF = myAlert.textFields![0] as UITextField
+            let aliasTF = myAlert.textFields![1] as UITextField
+            self.superheroes.append(superHeroTF.text!)
+            self.realNames.append(aliasTF.text!)
+            self.myTableView.reloadData()
+        }
+        myAlert.addAction(addAction)
+        self.presentViewController(myAlert, animated: true, completion: nil)
     }
 }
 
